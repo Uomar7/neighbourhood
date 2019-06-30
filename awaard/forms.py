41a01332ce79a30ea2bd9eb6fc881dd7558683aa
Uomar <uomarearlie7@gmail.com>
2019-06-30 17:23:16 +0300

    created the rating choices and modified the views

--- awaard/forms.py
+++ awaard/forms.py
@@ -1,31 +1,37 @@
 from django import forms
-from .models import Profile,Project,Comment,UsabilityRate,DesignRate,ContentRate
+from .models import Profile, Project, Comment, UsabilityRate, DesignRate, ContentRate
 from django.contrib.auth.models import User
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ["username"]
 
+
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        exclude = ['user','posted']
+        exclude = ['user', 'posted']
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        exclude = ['posted_by','project']
+        exclude = ['posted_by', 'project']
+
 
 class UsabilityRateForm(forms.ModelForm):
     class Meta:
         model = UsabilityRate
-        fields = ['rating]
+        fields = ['rating']
+
 
 class DesignRateForm(forms.ModelForm):
     class Meta:
         model = DesignRate
         fields = ['rating']
+
 
 class ContentRateForm(forms.ModelForm):
     class Meta:
--- awaard/models.py
+++ awaard/models.py
@@ -4,6 +4,7 @@ import datetime as dt
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class Profile(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
@@ -14,33 +15,62 @@ class Profile(models.Model):
     def __str__(self):
         return self.first_name
 
+
 class Project(models.Model):
-    title = models.CharField(max_length = 100)
-    project_image = models.ImageField(upload_to = 'images/', default = 'award/media/images')
+    title = models.CharField(max_length=100)
+    project_image = models.ImageField(upload_to='images/', default='award/media/images')
     project_description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     link = models.URLField()
     posted = models.DateTimeField(auto_now_add=True)
-    # new column comments added below 
+    # new column comments added below
+    # new column called usabilityrate
+
 
     def __str__(self):
         return self.title
 
+
 class Comment(models.Model):
-    review = models.CharField(max_length = 400)
-    project = models.ForeignKey(Project,on_delete=models.CASCADE,related_name='comments', related_query_name=comment) #automatically adds a new column on the project class called comments
-    posted_by = models.ForeignKey(User,on_delete=models.CASCADE)
+    review = models.CharField(max_length=400)# automatically adds a new column on the project class called comments
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='comments')
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.review
 
-class Rate(models.Model):
-    usability = models.IntegerField()
-    content = models.IntegerField()
-    design = models.IntegerField()
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name=rates)
-    voter = models.ForeignKey(User,on_delete=models.CASCADE)
+CHOICES = (
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+    (6, '6'),
+    (7, '7'),
+    (8, '8'),
+    (9, '9'),
+    (10, '10')
+)
 
+class DesignRate(models.Model):
+    project = models.ForeignKey(Project,related_name='design_rate')
+    voted_at = models.DateTimeField(auto_now_add=True)
+    voter = models.ForeignKey(User)
+    rating = models.IntegerField(choices=CHOICES, null=True)
+
+class ContentRate(models.Model):
+    project = models.ForeignKey(Project,related_name='content_rate')
+    voted_at = models.DateTimeField(auto_now_add=True)
+    voter = models.ForeignKey(User)
+    rating = models.IntegerField(choices=CHOICES, null=True)
+
+class UsabilityRate(models.Model):
+    project = models.ForeignKey(Project,related_name='usability_rate')
+    voted_at = models.DateTimeField(auto_now_add=True)
+    voter = models.ForeignKey(User)
+    rating = models.IntegerField(choices=CHOICES, null=True)
+
+    
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
