492f14f2b28ff004fef70de26acbae43e056ba67
Uomar <uomarearlie7@gmail.com>
2019-06-30 17:22:41 +0300

    modified my admin models

--- awaard/admin.py
+++ awaard/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Profile,Project,Comment,Rate
+from .models import Profile,Project,Comment,UsabilityRate,DesignRate,ContentRate
 
 admin.site.register(Profile)
 admin.site.register(Project)
 admin.site.register(Comment)
-admin.site.register(Rate)
+admin.site.register(UsabilityRate)
+admin.site.register(ContentRate)
+admin.site.register(DesignRate)
