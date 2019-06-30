d5aab7b20403abe894dd34a8f8cebbf2b58aa3f5
Uomar <uomarearlie7@gmail.com>
2019-06-30 17:17:44 +0300

    modified the rating forms

--- awaard/forms.py
+++ awaard/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Profile,Project,Comment,Rate
+from .models import Profile,Project,Comment,UsabilityRate,DesignRate,ContentRate
 from django.contrib.auth.models import User
 
 class ProfileForm(forms.ModelForm):
@@ -17,7 +17,17 @@ class CommentForm(forms.ModelForm):
         model = Comment
         exclude = ['posted_by','project']
 
-class RateForm(forms.ModelForm):
+class UsabilityRateForm(forms.ModelForm):
     class Meta:
-        model = Rate
-        exclude = ['project','voter']
+        model = UsabilityRate
+        fields = ['rating]
+
+class DesignRateForm(forms.ModelForm):
+    class Meta:
+        model = DesignRate
+        fields = ['rating']
+
+class ContentRateForm(forms.ModelForm):
+    class Meta:
+        model = ContentRate
+        fields = ['rating']
