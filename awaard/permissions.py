f7f2e98268f8121ec31e13441c26a74a6c1c398f
Uomar <uomarearlie7@gmail.com>
2019-07-01 10:05:18 +0300

    made APIView formmy models

--- awaard/permissions.py
+++ awaard/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
-
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
--- awaard/views.py
+++ awaard/views.py
@@ -7,6 +7,7 @@ from .models import Profile, Project, Comment, UsabilityRate, ContentRate, Desig
 from .forms import ProfileForm, CommentForm, ProjectForm, UsabilityRateForm,ContentRateForm,DesignRateForm
 from django.contrib.auth.models import User
 # creating an api view for my models.
+from .permissions import IsAdminOrReadOnly
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import ProfileSerializer,ProjectSerializer,CommentSerializer
@@ -19,20 +20,142 @@ def landing_page(request):
     return render(request, 'all-temps/index.html',{"projects":projects})
 
 class ProjectList(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+
     def get(self, request, format=None):
         all_projects = Project.objects.all()
         serializers = ProjectSerializer(all_projects, many=True)
 
         return Response(serializers.data)
+    
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data = request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProfileList(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+
     def get(self, request, format=None):
         all_Profiles = Profile.objects.all()
         serializers = ProfileSerializer(all_Profiles, many=True)
 
         return Response(serializers.data)
 
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class CommentList(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+
     def get(self, request, format=None):
         all_comments = Comment.objects.all()
         serializers = CommentSerializer(all_comments,many=True)
+
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProjectDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def get_project(self, pk):
+        try:
+            return Project.objects.get(pk = pk)
+        
+        except Project.DoesNotExist:
+            return Http404
+    
+    def put(self, request, pk, format=None):
+        project = self.get_project(pk)
+        serializers = ProjectSerializer(project, request.data)
+
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        project = self.get_project(pk)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ProfileDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def get_profile(self, pk):
+        try:
+            return Profile.objects.get(pk=pk)
+
+        except Profile.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        serializers = ProfileSerializer(profile)
+
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        serializers = ProfileSerializer(profile, request.data)
+
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def get_comment(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+
+        except Comment.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        comment = self.get_comment(pk)
+        serializers = CommentSerializer(comment)
+
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        comment = self.get_comment(pk)
+        serializers = CommentSerializer(comment, request.data)
+
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        comment = self.get_comment(pk)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
